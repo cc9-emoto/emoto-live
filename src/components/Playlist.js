9d2cd19c8c587e8227cbcde01c746a4d7b6cf30e
Wilson Lau <hello@wilsonplau.com>
2019-09-16 18:05:28 +0900

    Playlist styling

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,12 +1,18 @@
 import React  from 'react';
+import playOnly from '../assets/images/playOnly.svg'
 import "../styles/Playlist.scss"
 
 const Playlist = ({ upcoming, previous, current }) => {
   const playlistItem = (song) => {
     return (
       <div className="playlist__item" key={song.name}>
-        <h1>{ song.name }</h1>
-        <h1>{ song.artists[0].name }</h1>
+        <div className="playlist__itemLeft">
+        <img src={playOnly} />
+        </div>
+        <div className="playlist__itemRight">
+          <h2>{ song.name }</h2>
+          <h3>{ song.artists[0].name }</h3>
+        </div>
       </div>
     )
   }
@@ -14,6 +20,7 @@ const Playlist = ({ upcoming, previous, current }) => {
   return (
     <div className="playlist">
       <div className="playlist__top">
+        <h1>PLAYLIST</h1>
         {previous.map((song) => playlistItem(song))}
         { current && playlistItem(current) }
         {upcoming.map((song) => playlistItem(song))}
