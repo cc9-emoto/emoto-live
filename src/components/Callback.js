e0ca00ffe801cb64e8e57833acbc90d0a55a995c
Go Nakano <g.nakano555@gmail.com>
2019-09-23 19:41:18 +0900

    Update Callback.js

--- src/components/Callback.js
+++ src/components/Callback.js
@@ -9,24 +9,25 @@ const Callback = ({ location }) => {
   const code = location.search;
 
   useEffect(() => {
-    const getTokens = async () => {
-      const res = await axios.get(
-        `https://hur7tfyff1.execute-api.us-east-2.amazonaws.com/Prod/spotify/callback${code}`,
-        {
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      );
-      Cookies.set("emoto-access", res.data.access_token);
-      Cookies.set("emoto-refresh", res.data.refresh_token);
-      Cookies.set("emoto-id", res.data.uid);
-      setLoaded(true);
-      return;
-    };
     getTokens();
     return;
   }, []);
+
+  const getTokens = async () => {
+    const res = await axios.get(
+      `https://hur7tfyff1.execute-api.us-east-2.amazonaws.com/Prod/spotify/callback${code}`,
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+    Cookies.set("emoto-access", res.data.access_token);
+    Cookies.set("emoto-refresh", res.data.refresh_token);
+    Cookies.set("emoto-id", res.data.uid);
+    setLoaded(true);
+    return;
+  };
 
   if (loaded) return <Redirect to="/dashboard" />;
   else {
